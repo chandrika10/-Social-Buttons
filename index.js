8629ef4a1dc100bfaacafc71660d4692033690c4
chandrika <chandrikavangapandu@gmail.com>
2023-04-02 05:32:28 +0000

    Social Buttons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Button = (props) => {
   //  Write your code here.
   const { className, buttonText } = props;
-  return <Button className={"button ${className}"}>{buttonText}</Button>;
+  return <button className={"button ${className}"}>{buttonText}</button>;
 };
 
 const element = (
@@ -9,9 +9,9 @@ const element = (
   <div className="big-container">
     <h1 className="heading">Social Buttons</h1>
     <div className="buttons-container">
-      <Button buttonText="Like" className="like-btn" />
-      <Button buttonText="Comment" className="comment-btn" />
-      <Button buttonText="Share" className="share-btn" />
+      <Button className="like-button" buttonText="Like" />
+      <Button className="comment-button" buttonText="Comment" />
+      <Button className="share-button" buttonText="Share" />
     </div>
   </div>
 );
